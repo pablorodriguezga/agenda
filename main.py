199146b505d374ec90ba51a170e190fc87115674
unknown <prguezgh@gmail.com>
2023-08-10 14:31:11 +0200

    Cors

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask, jsonify, request
-
+from flask_cors import CORS
 from routes.userRoutes import user_api
 from routes.contactRoutes import contact_api
 
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(user_api)
 app.register_blueprint(contact_api)
